7215d0408917acf8a3d42f551acb4dc521f071c4
mkdh <mkdh321@yahoo.com.tw>
2017-04-27 14:49:30 +0800

    [update] using read all instead of read line
    [update] using read all instead of read line.
    Reference.
    http://stackoverflow.com/questions/15115571/reading-a-txt-file-using-qte
    xtstream-c

--- cls_file_io.cpp
+++ cls_file_io.cpp
@@ -27,14 +27,17 @@ QString clsFileIO::read()
 
     QFile file(mSource);
     QString fileContent;
-    if ( file.open(QIODevice::ReadOnly) ) {
-        QString line;
+    if ( file.open(QIODevice::ReadOnly) )
+    {
         QTextStream t( &file );
+        /*
+        QString line;
         do {
             line = t.readLine();
             fileContent += line + "\r\n";
         } while (!line.isNull());
-
+        */
+        fileContent = t.readAll();
         file.close();
     } else {
         emit error("Unable to open the file");
